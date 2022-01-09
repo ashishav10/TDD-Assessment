ae805596538597f072cbdd78fe4ec9e73f16fa04
Ashish Vaishnav <ashishvaishnav481929@gmail.com>
2022-01-09 16:40:00 +0530

    Added test for new line char as delimiter

--- src/com/tdd/calculator/Calculator.java
+++ src/com/tdd/calculator/Calculator.java
@@ -3,17 +3,25 @@ package com.tdd.calculator;
 public class Calculator {
 
 	public static int add(String input) {
-		
+
 		int sum = 0;
 //		Empty String
-		if(input.isBlank()) {
+		if (input.isBlank()) {
 			return 0;
+		} else {
+			String[] numbers = splitUtil(input);
+			return sumUtil(numbers);
 		}
-		else {
-			String[] numbers = input.split(",");
-			for(String num: numbers)
-				sum += Integer.parseInt(num);
-			return sum;
-		}
+	}
+
+	private static String[] splitUtil(String input) {
+		return input.split(",|\n");
+	}
+
+	private static int sumUtil(String[] numbers) {
+		int result = 0;
+		for (String num : numbers)
+			result += Integer.parseInt(num);
+		return result;
 	}
 }
--- src/com/tdd/calculator/CalculatorTest.java
+++ src/com/tdd/calculator/CalculatorTest.java
@@ -23,4 +23,10 @@ public class CalculatorTest {
 	public void delimitedByComma() {
 		assertEquals(10, Calculator.add("5,3,2"));
 	}
+
+//	Numbers delimited by newline char
+	@Test
+	public void delimitedByNewLineChar() {
+		assertEquals(10, Calculator.add("5\n2,3"));
+	}
 }
